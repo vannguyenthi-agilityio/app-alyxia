2d051d0fae5aabd13f234a66067d5be62d35b87b
Van Nguyen <van.nguyenthi@asnet.com.vn>
2022-12-08 14:35:49 +0700

    feat: use let instead var

--- js/video-hero.js
+++ js/video-hero.js
@@ -1,16 +1,16 @@
-var tag = document.createElement('script');
+let tag = document.createElement('script');
 
 // 2. This code loads the IFrame Player API code asynchronously.
 tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-var firstScriptTag = document.getElementsByTagName('script')[0];
+let firstScriptTag = document.getElementsByTagName('script')[0];
+let firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 // 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-var player;
-var playButton = document.getElementById("play");
-var pauseButton = document.getElementById("close");
-var iframe = document.getElementById("player");
+// after the API code downloads.
+let player;
+let playButton = document.getElementById("play");
+let pauseButton = document.getElementById("close");
+let iframe = document.getElementById("player");
 playButton.addEventListener("click", play);
 pauseButton.addEventListener("click",pause);
 iframe.setAttribute('src', 'https://www.youtube.com/embed/EwOuGsrYmGA?enablejsapi=1');
@@ -19,18 +19,16 @@ function onYouTubeIframeAPIReady() {
     videoId: 'EwOuGsrYmGA',
     events: {
       'onReady': onPlayerReady
-//      'onStateChange': onPlayerStateChange
     }
   });
 }
 // 4. The API will call this function when the video player is ready.
-  function onPlayerReady(event) {
-//    play();
-
-  }
-  function pause() {
-    player.pauseVideo();
-  }
-  function play() {
-  	player.playVideo();
-  }
+// function onPlayerReady(event) {
+//   // play();
+// }
+function pause() {
+  player.pauseVideo();
+}
+function play() {
+  player.playVideo();
+}
