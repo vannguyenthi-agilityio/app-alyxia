e1c407f378bae8d6a07a275b4f8aaa1353982654
Ni Ngo <ni.ngo@asnet.com.vn>
2022-12-14 14:37:46 +0700

    Handle to add iframe

--- js/video-hero.js
+++ js/video-hero.js
@@ -1,34 +1,72 @@
 let tag = document.createElement('script');
-
-// 2. This code loads the IFrame Player API code asynchronously.
-tag.src = "https://www.youtube.com/iframe_api";
 let firstScriptTag = document.getElementsByTagName('script')[0];
-let firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-// 3. This function creates an <iframe> (and YouTube player)
-// after the API code downloads.
 let player;
 let playButton = document.getElementById("play");
 let pauseButton = document.getElementById("close");
-let iframe = document.getElementById("player");
+let iframeWrapper = document.getElementById("banner-video");
+let iframe = document.getElementById("iframe");
+
+// 2. This code loads the IFrame Player API code asynchronously.
+tag.src = "https://www.youtube.com/iframe_api";
+
+// 3. This function creates an <iframe> (and YouTube player)
+// after the API code downloads.
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 playButton.addEventListener("click", play);
-pauseButton.addEventListener("click",pause);
-iframe.setAttribute('src', 'https://www.youtube.com/embed/EwOuGsrYmGA?enablejsapi=1');
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    videoId: 'EwOuGsrYmGA',
-    events: {
-      'onReady': onPlayerReady
-    }
-  });
-}
-// 4. The API will call this function when the video player is ready.
-// function onPlayerReady(event) {
-//   // play();
+pauseButton.addEventListener("click", pause);
+
+
+
+{/* <iframe id="player" class="vid" width="100%" height="100%" 
+src="https://www.youtube.com/embed/ci9QZbNLct0?enablejsapi=1" 
+frameborder="0" 
+allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+allowfullscreen=""></iframe> */}
+
+// function onYouTubeIframeAPIReady() {
+  
 // }
-function pause() {
-  player.pauseVideo();
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  console.log('onPlayerReady')
+  player = event.target;
+  event.target.playVideo();
 }
+
+function pause() {
+  console.log(player)
+  if (player && player.pauseVideo) {
+    player.pauseVideo();
+  }
+}
+
 function play() {
-  player.playVideo();
+  if(!iframe) {
+    iframe = document.createElement("iframe");
+    iframe.onload = function() {
+      window.onYouTubeIframeAPIReady = () => {
+        player = new YT.Player('banner-video', {
+          videoId: 'EwOuGsrYmGA',
+          events: {
+            'onReady': onPlayerReady
+          }
+        });
+      }
+    }; 
+    iframe.setAttribute('src', 'https://www.youtube-nocookie.com/embed/EwOuGsrYmGA?enablejsapi=1');
+    iframe.setAttribute('allowfullscreen', '');
+    iframe.setAttribute('allow', 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture');
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('id', 'player');
+    iframe.setAttribute('class', 'vid');
+    iframe.setAttribute('width', '100%');
+    iframe.setAttribute('height', '100%');
+    iframe.setAttribute('title', 'AgilityIO - New Yorks Enterprise Web and Mobile Development Agency');
+    iframeWrapper.appendChild(iframe)
+    iframe.focus();
+  }
+  if (player && player.playVideo) {
+    player.playVideo();
+  }
 }
