876f9b8b51d54fb0191bd2998cc2a9e461338952
Ni Ngo <ni.ngo@asnet.com.vn>
2022-12-14 15:10:01 +0700

    Fix video loading and auto play

--- js/video-hero.js
+++ js/video-hero.js
@@ -4,7 +4,7 @@ let player;
 let playButton = document.getElementById("play");
 let pauseButton = document.getElementById("close");
 let iframeWrapper = document.getElementById("banner-video");
-let iframe = document.getElementById("iframe");
+let iframe = document.getElementById("player");
 
 // 2. This code loads the IFrame Player API code asynchronously.
 tag.src = "https://www.youtube.com/iframe_api";
@@ -15,27 +15,13 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 playButton.addEventListener("click", play);
 pauseButton.addEventListener("click", pause);
 
-
-
-{/* <iframe id="player" class="vid" width="100%" height="100%" 
-src="https://www.youtube.com/embed/ci9QZbNLct0?enablejsapi=1" 
-frameborder="0" 
-allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-allowfullscreen=""></iframe> */}
-
-// function onYouTubeIframeAPIReady() {
-  
-// }
-
 // 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
-  console.log('onPlayerReady')
   player = event.target;
   event.target.playVideo();
 }
 
 function pause() {
-  console.log(player)
   if (player && player.pauseVideo) {
     player.pauseVideo();
   }
@@ -43,27 +29,27 @@ function pause() {
 
 function play() {
   if(!iframe) {
+    // Create iframe once loading
     iframe = document.createElement("iframe");
-    iframe.onload = function() {
-      window.onYouTubeIframeAPIReady = () => {
-        player = new YT.Player('banner-video', {
-          videoId: 'EwOuGsrYmGA',
-          events: {
-            'onReady': onPlayerReady
-          }
-        });
-      }
-    }; 
+    iframe.allowfullscreen = '';
+    iframe.allow = 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture';
+    iframe.frameborder = '0';
+    iframe.id = 'player';
+    iframe.class = 'vid';
+    iframe.width = '100%';
+    iframe.height = '100%';
+    iframe.title = 'AgilityIO - New Yorks Enterprise Web and Mobile Development Agency';
+    iframe.style = 'border: 0';
     iframe.setAttribute('src', 'https://www.youtube-nocookie.com/embed/EwOuGsrYmGA?enablejsapi=1');
-    iframe.setAttribute('allowfullscreen', '');
-    iframe.setAttribute('allow', 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture');
-    iframe.setAttribute('frameborder', '0');
-    iframe.setAttribute('id', 'player');
-    iframe.setAttribute('class', 'vid');
-    iframe.setAttribute('width', '100%');
-    iframe.setAttribute('height', '100%');
-    iframe.setAttribute('title', 'AgilityIO - New Yorks Enterprise Web and Mobile Development Agency');
     iframeWrapper.appendChild(iframe)
+    
+    // Initialize player to control when play/pause
+    new YT.Player('player', {
+      videoId: 'EwOuGsrYmGA',
+      events: {
+        'onReady': onPlayerReady
+      }
+    });
     iframe.focus();
   }
   if (player && player.playVideo) {
