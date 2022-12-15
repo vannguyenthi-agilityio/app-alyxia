a540faaa3237e6a7b2f29100faf882de02646cae
Van Nguyen <van.nguyenthi@asnet.com.vn>
2022-12-16 01:50:51 +0700

    feat: check to add  recaptcha

--- js/contact.js
+++ js/contact.js
@@ -1,12 +1,7 @@
 let recaptchaContactForm = document.getElementById("recaptcha-contact-form");
 let contactUs = document.getElementById("contact-us");
-
-function setRecaptcha() {
-  if(!recaptchaContactForm) {
-    divRecaptcha = document.createElement("div");
-    divRecaptcha.setAttribute('data-netlify-recaptcha', 'true');
-    recaptchaContactForm.appendChild(divRecaptcha);
-  }
+if(!recaptchaContactForm) {
+  divRecaptcha = document.createElement("div");
+  divRecaptcha.setAttribute('data-netlify-recaptcha', 'true');
+  recaptchaContactForm.appendChild(divRecaptcha);
 }
-
-contactUs.addEventListener("click", setRecaptcha);
