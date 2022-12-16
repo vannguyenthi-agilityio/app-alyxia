3a80c0db1fec4ee1c52118f670734f1271c0ad5b
Van Nguyen <van.nguyenthi@asnet.com.vn>
2022-12-16 12:19:02 +0700

    feat: change images

--- js/contact.js
+++
@@ -1,5 +0,0 @@
-let recaptchaContactForm = document.getElementById("recaptcha-contact-form");
-divRecaptcha = document.createElement("div");
-divRecaptcha.setAttribute('data-netlify-recaptcha', 'true');
-recaptchaContactForm.appendChild(divRecaptcha);
-divRecaptcha.focus();
